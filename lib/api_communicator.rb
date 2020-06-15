20759ef97e3934faed687c99dca9816a12b6ff26
DannyCoop <cooperdaniel5141@gmail.com>
2020-06-15 14:18:20 -0400

    nothing

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -1,14 +1,14 @@
 require 'rest-client'
 require 'json'
 require 'pry'
+require 'pry'
 
 def get_character_movies_from_api(character_name)
   #make the web request
 
-
   response_string = RestClient.get('http://swapi.dev/api/people')
   response_hash = JSON.parse(response_string)
-
+  
   # iterate over the response hash to find the collection of `films` for the given
   #   `character`
   # collect those film API urls, make a web request to each URL to get the info
@@ -33,3 +33,5 @@ end
 
 # that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
 # can you split it up into helper methods?
+
+get_character_movies_from_api()
